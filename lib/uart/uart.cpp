e056e167aac08f62d61292260df0e8e917327345
Jay <33371449+jay94ks@users.noreply.github.com>
2024-02-07 02:05:18 +0900

    `UART` 기능 구현시 사용하는 기반 클래스.

---
+++ lib/uart/uart.cpp
@@ -0,0 +1,142 @@
+#include "uart.h"
+
+class uart_intr {
+public:
+
+    /**
+     * proxy ERROR interrupt call to UART instance.
+     */
+    static void proxy_error(huart_t huart) {
+        auto uart = uart_t::root();
+        while(uart) {
+            if (uart->handle() != huart) {
+                uart = uart->link();
+                continue;
+            } 
+
+            uart->on_error();
+            break;
+        }
+    }
+
+    /**
+     * proxy RX interrupt call to UART instance.
+     */
+    static void proxy_rx(huart_t huart) {
+        auto uart = uart_base_t::root();
+        while(uart) {
+            if (uart->handle() != huart) {
+                uart = uart->link();
+                continue;
+            } 
+
+            uart->on_recv();
+            break;
+        }
+    }
+};
+
+extern "C" {
+	void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+		uart_intr::proxy_rx(huart);
+	}
+
+	void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
+		uart_intr::proxy_error(huart);
+	}
+}
+
+// --
+uart_base_t* uart_base_t::_root = nullptr;
+
+uart_base_t::uart_t(uart_base_t uart)
+    : _uart(uart), _link(nullptr), _intr(0)
+{
+}
+
+bool uart_base_t::begin_intr(void* buf, uint32_t length) {
+    if (_intr & INTR_EN) {
+        return false;
+    }
+
+    _intr |= INTR_EN;
+    _intr &= ~INTR_RX;
+    _intr &= ~INTR_ERR;
+
+    if (HAL_UART_Receive_IT(_uart, (uint8_t*) buf, length) != HAL_OK) {
+        _intr &= ~INTR_EN;
+        _intr |= INTR_ERR;
+        return false;
+    }
+
+    return true;
+}
+
+bool uart_base_t::enable() {
+    auto temp = _root;
+    while(temp) {
+        if (temp == this) {
+            return false;
+        }
+
+        temp = temp->_link;
+    }
+
+    if (!_root) {
+        _root = this;
+    } 
+    
+    else {
+        _link = _root;
+        _root = this;
+    }
+
+    return true;
+}
+
+bool uart_base_t::disable() {
+    if (_root == this) {
+        _root = _link;
+        _link = nullptr;
+        return true;
+    }
+
+    auto temp = _root;
+    while(temp) {
+        if (temp->_link == this) {
+            temp->_link = _link;
+            _link = nullptr;
+            return true;
+        }
+
+        temp = temp->_link;
+    }
+
+    return false;
+}
+
+bool uart_base_t::wait(uint32_t timeout) {
+    if (!intr_en()) {
+        return false;
+    }
+
+    if (intr_in()) {
+        return true;
+    }
+
+    uint32_t ticks = HAL_GetTick();
+    while(true) {
+        if (intr_rx()) {
+            return true;
+        }
+
+        if (intr_err()) {
+            return true;
+        }
+
+        uint32_t now = HAL_GetTick();
+        if ((now - ticks) >= timeout) {
+            return false;
+        }
+    }
+}
---
+++ lib/uart/uart.h
@@ -0,0 +1,145 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+#include "main.h"
+
+// --> shortcut.
+using huart_t = UART_HandleTypeDef*;
+
+// --> forward decl: uart.cpp.
+class uart_intr;
+
+/**
+ * Describes an UART communication port.
+ * warning: do not copy this instance anyway.
+ * because, the interrupt will not be passed to last initialized instance.
+ */
+class uart_base_t {
+    friend class uart_intr;
+
+private:
+    huart_t _uart;
+    uint8_t _intr;
+
+private:
+    static constexpr uint8_t INTR_EN = 0x80;
+    static constexpr uint8_t INTR_IN = 0x40;
+
+    static constexpr uint8_t INTR_RX = 0x01;
+    static constexpr uint8_t INTR_ERR = 0x02;
+
+protected:
+    static uart_base_t* _root;
+    uart_base_t* _link;
+
+public:
+    /**
+     * initialize an uart_t using its handle.
+     */
+    uart_base_t(huart_t uart);
+
+    /**
+     * de-initialize the uart_t.
+     * this will unlink self from root.
+     */
+    virtual ~uart_base_t() { disable(); }
+
+protected:
+    /* get the root instance. */
+    inline static uart_base_t* root() const { return _root; }
+
+    /* get the linked instance. */
+    inline uart_base_t* link() const { return _link; }
+    
+protected:
+    /**
+     * called when `error` interrupt occurred.
+     */
+    virtual void on_error() {
+        _intr |= INTR_ERR | INTR_IN; 
+
+        if (_intr & INTR_EN) {
+            _intr &= ~INTR_EN;
+            
+            on_read_intr();
+        }
+
+        _intr &= ~INTR_IN;
+    }
+
+    /**
+     * (abstract) called when `recv` interrupt occurred.
+     * do not do complex something in this method.
+     */
+    virtual void on_recv() { 
+        _intr |= INTR_RX | INTR_IN; 
+
+        if (_intr & INTR_EN) {
+            _intr &= ~INTR_EN;
+
+            on_read_intr();
+        }
+
+        _intr &= ~INTR_IN;
+    }
+    
+protected:
+    /**
+     * begin the interrupt based recv and enable interrupt.
+     * call this from child class that inherited this class.
+     */
+    bool begin_intr(void* buf, uint32_t length);
+
+    /**
+     * called from interrupt to renew buffer.
+     * this must call `begin_intr` to receive more bytes.
+     */
+    virtual void on_read_intr() { }
+
+public:
+    /**
+     * get the handle.
+     */
+    inline huart_t handle() const { return _uart; }
+
+    /* test whether the UART instance is running under interrupt mode or not. */
+    inline bool intr_en() const { return _intr & INTR_EN; }
+
+    /* test whether the RX interrupt triggered or not. */
+    inline bool intr_rx() const { return _intr & INTR_RX; }
+
+    /* test whether the ERR interrupt triggered or not. */
+    inline bool intr_err() const { return _intr & INTR_ERR; }
+
+    /* test whether the code is running under interrupt or not. */
+    inline bool intr_in() const { return _intr & INTR_IN; }
+
+    /**
+     * enable this instance.
+     * actually, link this instance to root.
+     */
+    virtual bool enable();
+
+    /**
+     * disable this instance.
+     * actually, unlink this instance from root.
+     */
+    virtual bool disable();
+
+    /**
+     * wait for RX/ERR interrupt.
+     */
+    bool wait(uint32_t timeout = 0xffffffffu);
+
+    /**
+     * write the buffer through UART port.
+     * returns false if timeout reached or error.
+     */
+    inline bool write_b(const void* buf, uint32_t length, uint32_t timeout = 0xffffffffu) {
+        return HAL_UART_Transmit(_uart, (uint8_t*) buf, length, timeout);
+    }
+
+};
+
+
+#endif // __UART_H__
