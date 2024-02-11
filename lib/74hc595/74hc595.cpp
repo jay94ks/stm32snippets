9244c35ef9cd82edd166ad06b675d45c2d0e886b
Jay <33371449+jay94ks@users.noreply.github.com>
2024-02-11 14:28:35 +0900

    다수의 `74HC595`칩이 직렬 연결되었을 때 처리 추가.

--- lib/74hc595/74hc595.cpp
+++ lib/74hc595/74hc595.cpp
@@ -1,12 +1,31 @@
 #include "74hc595.h"
 
-l74hc595_t::l74hc595_t(pin_t data, pin_t clock, pin_t latch)
-    : _data(data), _clock(clock), _latch(latch),
-        _state(0), _value(0)
+l74hc595_ctl_t::l74hc595_ctl_t(const pin_t& data, const pin_t& clock, const pin_t& latch)
+    : _data(data), _clock(clock), _latch(latch), _state(0)
 {
+    _latch.write(low);
 }
 
-bool l74hc595_t::begin() {
+void l74hc595_ctl_t::xfer(uint8_t value) {
+    for(uint8_t i = 0; i < 8; ++i) {
+        pin_state_t state = (value & (1 << i)) ? high : low;
+
+        _data.write(state);
+        _clock.write(high);
+
+        pin_t::delay();
+        _clock.write(low);
+    }
+}
+
+void l74hc595_ctl_t::latch() {
+    _latch.write(high);
+
+    pin_t::delay();
+    _latch.write(low);
+}
+
+bool l74hc595_ctl_t::begin() {
     if (_state) {
         return false;
     }
@@ -15,49 +34,23 @@ bool l74hc595_t::begin() {
     return true;
 }
 
-void l74hc595_t::end(bool flush) {
+bool l74hc595_ctl_t::end() {
     if (!_state) {
-        return;
+        return false;
     }
 
     _state = 0;
-    if (flush) {
-        this->flush();
-    }
+    return true;
 }
 
-void l74hc595_t::flush() {
-    _latch.write(low);
+void l74hc595_write_bit(uint8_t* buf, uint8_t pin, pin_state_t state) {
+    const uint8_t n = pin / 8;
 
-    for(uint8_t i = 0; i < 8; ++i) {
-        pin_state_t state = (_value & (1 << i)) ? high : low;
-
-        _data.write(state);
-        _clock.write(high);
-
-        pin_t::delay();
-        _clock.write(low);
-    }
-
-    _latch.write(high);
-}
-
-pin_state_t l74hc595_t::read(uint8_t pin) {
-    return (_value & (1 << pin)) ? high : low;
-}
-
-l74hc595_t& l74hc595_t::write(uint8_t pin, pin_state_t state) {
     if (state) {
-        _value |= 1 << pin;
+        buf[n] |= 1 << pin;
     }
 
     else {
-        _value &= ~(1 << pin);
+        buf[n] &= ~(1 << pin);
     }
-
-    if (!_state) {
-        flush();
-    }
-
-    return *this;
 }
--- lib/74hc595/74hc595.h
+++ lib/74hc595/74hc595.h
@@ -4,37 +4,105 @@
 // --> PIN library.
 #include "../pin/pin.h"
 
+// --> memset(...).
+#include <string.h>
+
 /**
- * Describes 74HC595 bit-shift register.
+ * Describes a 74HC595 controller.
  */
-class l74hc595_t {
+class l74hc595_ctl_t {
 private:
     pin_t _data;
     pin_t _clock;
     pin_t _latch;
-
     uint8_t _state;
-    uint8_t _value;
 
 public:
-    l74hc595_t() : _state(0), _value(0) { }
-    l74hc595_t(pin_t data, pin_t clock, pin_t latch);
+    l74hc595_ctl_t() : _state(0) { }
+    l74hc595_ctl_t(const pin_t& data, const pin_t& clock, const pin_t& latch);
+
+public:
+    /* transmit bits. */
+    void xfer(uint8_t value);
+
+    /* trigger the latch. */
+    void latch();
+    
+    /* begin the state to optimise IO clock. */
+    bool begin();
+
+    /* end the state. */
+    bool end();
+
+    /* access the state. */
+    inline uint8_t state() const { return _state; }
+};
+
+/* write bit to state buffer. */
+void l74hc595_write_bit(uint8_t* buf, uint8_t pin, pin_state_t state);
+
+/**
+ * Describes 74HC595 bit-shift register.
+ */
+template<uint32_t pcs = 1>
+class g74hc595_t {
+private:
+    l74hc595_ctl_t _ctl;
+    uint8_t _value[pcs];
+
+public:
+    g74hc595_t() {
+        memset(_value, 0, sizeof(_value));
+    }
+
+    g74hc595_t(pin_t data, pin_t clock, pin_t latch)
+        : _ctl(data, clock, latch)
+    {
+        memset(_value, 0, sizeof(_value));
+    }
 
 public:
     /* begin the state cache to optimise IO clock. */
-    bool begin();
+    inline bool begin() { return _ctl.begin(); }
 
     /* end the state cache. */
-    void end(bool flush = true);
+    inline void end(bool flush = true) {
+        if (!_ctl.end()) {
+            return;
+        }
+        
+        if (flush) {
+            this->flush();
+        }
+    }
 
     /* flush the state cache to physical chipset. */
-    void flush();
+    void flush() {
+        for(uint8_t i = 0; i < pcs; ++i) {
+            _ctl.xfer(_value[i]);
+        }
+
+        _ctl.latch();
+    }
 
     /* read a pin from state cache. */
-    pin_state_t read(uint8_t pin);
+    pin_state_t read(uint8_t pin) {
+        return (_value[pin / 8] & (1 << pin)) ? high : low;
+    }
 
     /* write a pin into state cache or direct out. */
-    l74hc595_t& write(uint8_t pin, pin_state_t state);
+    g74hc595_t& write(uint8_t pin, pin_state_t state) {
+        l74hc595_write_bit(_value, pin, state);
+
+        if (!_ctl.state()) {
+            flush();
+        }
+
+        return *this;
+    }
 };
+
+/* 74HC595, 1 pcs. */
+using l74hc595_t = g74hc595_t<1>;
 
 #endif // __74HC595_H__
